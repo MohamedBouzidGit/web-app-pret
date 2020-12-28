a885b7803470dc232a5f600b0d10a3902f2eb15e
MohamedBouzidGit <mohamed.bouzid@tutamail.com>
2020-12-28 18:15:07 +0100

    Mise à jour 8

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import plotly.graph_objects as go
 import pickle
 from sklearn.neighbors import KDTree
-import request
+import requests
 import json
 import Flask
 from sklearn.ensemble import GradientBoostingClassifier
